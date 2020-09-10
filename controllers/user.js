68a21ce3cc42d2316b09a9f4c63542af33349ef7
tochukwu <teeceemum@gmail.com>
2020-09-10 17:00:19 +0100

    user update

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user')
+const {Order } = require('../models/order')
+const { errorHandler } = require('../helpers/dberrorHandler')
 
 exports.userById = (req,res,next,id) =>{
     User.findById(id).exec((err,user)=>{
@@ -52,4 +54,15 @@ exports.populateUserHistorywithOrder = (req,res,next) =>{
         }
         next()
     })
+}
+exports.purchaseHistory = (req,res) =>{
+    Order.find({user:req.profile._id}).populate('user','_id name').sort('-created').exec((err,orders) =>{
+        if(err){
+            return res.status(400).json({
+                error:errorHandler(err)
+            })
+        }
+        res.json(orders)
+    })
+
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const {checkToken,authAdminUser,authregUser} = require('../controllers/auth.js')
-const {userById,getUser,updateUser} = require('../controllers/user.js')
+const {userById,getUser,updateUser,purchaseHistory} = require('../controllers/user.js')
 
 const router = express.Router();
 router.get('/user/:userId',checkToken,authregUser,getUser)
 router.put('/user/:userId',checkToken,authregUser,updateUser)
+router.get('/user/orders/by/user/:userId',checkToken,authregUser,purchaseHistory)
 router.param("userId",userById)
   
 
